7f2bf8d96b4e7f26d2685d090f183dd634389a9c
Julian zapata rugeles <juliang.@@>
2020-09-12 21:42:06 -0500

    problema 3 _ avance

---
+++ 3_problema/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned short int mounths[12]={31,29,31,30,31,30,31,31,30,31}; // año 2020
+    unsigned short int date[]={0,0,0};
+    bool slashFound=false;
+    char userInput[4]={'!','!','!','!'};
+    cout << "Ingrese el formato dia/mes : " ; cin >> userInput ;
+    return 0;
+    }
+
